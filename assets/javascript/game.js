025d55d79749f7782702aa4ce106cdf303130cdf
ATotti <amirhasan.jafari@philips.com>
2018-05-07 11:19:50 -0400

    click player done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,32 @@
 
 //Defining array with words to guess
-var soccerPlayers = ["Baggio", "Totti", "Zidane", "Rivaldo", "Ronaldo"]
+var soccerPlayers = ["Baggio", "Totti", "Zidane", "Rivaldo", "Ronaldinho"]
 
-$("#startButton").on("click", function () {
-    var number = Math.floor((Math.random() * soccerPlayers.length));
-    $("#playerText").text(soccerPlayers[number])
-})
+
+for (i = 0; i < soccerPlayers.length; i++) {
+    console.log(soccerPlayers[i].length);
+}
+    //$("#startButton").on("click", function () {
+    // var number = Math.floor((Math.random() * soccerPlayers.length));
+    //})
+
+
+    document.getElementById("startButton").onclick = function() {pickPlayer()};
+
+    function pickPlayer(){
+        var str = "_ ";
+        var number = soccerPlayers[Math.floor((Math.random() * soccerPlayers.length))];
+        console.log (number)
+        document.getElementById("playerText").innerHTML = str.repeat(number.length);
+    }
+
+
+    // var str = "_ ";
+    // for (i = 0; i < soccerPlayers.length; i++) {
+    //     document.getElementById("playerText").innerHTML = str.repeat(soccerPlayers[i].length)
+    //     //$("#playerText").text(str.repeat(soccerPlayers[i].length));
+    // }
+
 
 
 
